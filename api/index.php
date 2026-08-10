86eb158540953e4d7e160c808e21fc393abe9194
mac <mac@macs-MacBook-Air.local>
2026-08-10 09:19:49 +0700

    Fix SCRIPT_NAME routing for Vercel serverless PHP

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+
 // Forward all requests to Laravel public/index.php
 require __DIR__ . '/../public/index.php';
+
