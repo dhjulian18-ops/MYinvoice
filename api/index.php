f0a13ba6f1cc18d4000b383e125d9624a07acf85
mac <mac@macs-MacBook-Air.local>
2026-08-10 09:20:26 +0700

    Set apiPrefix to empty string for Vercel deployment

--- api/index.php
+++ api/index.php
@@ -1,8 +1,6 @@
 <?php
 
-$_SERVER['SCRIPT_NAME'] = '/index.php';
-$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
-
 // Forward all requests to Laravel public/index.php
 require __DIR__ . '/../public/index.php';
+
 
